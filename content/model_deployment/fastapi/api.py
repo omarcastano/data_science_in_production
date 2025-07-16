de710a6a4a5149b8a0a36629b636ffc6abcf6aa8
Omar Andres Castaño <omar.castano@udea.edu.co>
2025-07-16 17:49:24 -0500

    feat: update FastAPI example API implementation

--- content/model_deployment/fastapi/api.py
+++ content/model_deployment/fastapi/api.py
@@ -8,10 +8,10 @@ app = FastAPI()
 @app.get("/get_fraction")
 def get_fraction(a: float, b: float) -> Dict:
     if a is None or b is None:
-        return {"error": "Both 'a' and 'b' must be provided."}
+        raise ValueError("Both 'a' and 'b' must be provided.")
 
     if b == 0:
-        return {"error": "Division by zero is not allowed."}
+        raise ValueError("Division by zero is not allowed.")
 
     result = a / b
     return {"result": result}
